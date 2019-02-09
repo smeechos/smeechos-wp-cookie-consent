84f608abf7c2755caf1e874e198ebec66ffb56ac
Alex DeRito <20422775+smeechos@users.noreply.github.com>
2019-02-09 17:09:23 -0500

    Setting up Gulp w/ Sass and JS

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { series } = require( 'gulp' );
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let rename = require('gulp-rename');
+let uglify = require('gulp-uglify');
+let cleanCSS = require('gulp-clean-css');
+let sourcemaps = require('gulp-sourcemaps');
+
+sass.compiler = require('node-sass');
+
+function complieSass(cb) {
+    return gulp.src('./assets/css/src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css/src'));
+}
+
+function minifyCSS(cb) {
+    return gulp.src('./assets/css/src/styles.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./assets/css/dist'));
+}
+
+function minifyJS(cb) {
+    return gulp.src('./assets/js/src/scripts.js')
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./assets/js/dist'));
+}
+
+
+exports.default = series(complieSass, minifyCSS, minifyJS);
